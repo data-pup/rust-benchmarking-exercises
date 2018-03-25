f83b5e260bcf2743480fad30d1c1931cb5cb4a7f
data-pup <kevinmartinucr@gmail.com>
2018-03-24 18:37:39 -0700

    Added some basic tests to make sure a default queue is set up as expected.

--- src/priority_queue_ll/mod.rs
+++ src/priority_queue_ll/mod.rs
@@ -13,25 +13,25 @@ struct LlNode<V, P>
 }
 
 /// Priority Queue, using a linked list implementation.
-pub struct PriorityQueueLinkedList<V, P>
+pub struct PQueueLl<V, P>
     where P:PartialOrd
 {
     head:Option<Box<LlNode<V, P>>>,
     queue_type:QueueType,
 }
 
-impl<V, P> PriorityQueueLinkedList<V, P>
+impl<V, P> PQueueLl<V, P>
     where P: PartialOrd
 {
     pub fn new(queue_type:QueueType) -> Self {
-        PriorityQueueLinkedList {
+        PQueueLl {
             head:None,
             queue_type:queue_type,
         }
     }
 }
 
-impl<V, P> Queue<V, P> for PriorityQueueLinkedList<V, P>
+impl<V, P> Queue<V, P> for PQueueLl<V, P>
     where P: PartialOrd
 {
     fn push(&mut self, value:V, priority:P) {
@@ -48,15 +48,26 @@ impl<V, P> Queue<V, P> for PriorityQueueLinkedList<V, P>
 
 #[cfg(test)]
 mod tests {
+    use Queue;
     use QueueType;
     use priority_queue_ll;
-    use priority_queue_ll::PriorityQueueLinkedList;
+    use priority_queue_ll::PQueueLl;
+
+    fn create_empty_u32_max_queue() -> PQueueLl<u32, u32> { return PQueueLl::new(QueueType::MaxQueue); }
+    fn create_empty_u32_min_queue() -> PQueueLl<u32, u32> { return PQueueLl::new(QueueType::MinQueue); }
 
     #[test]
     fn create_empty_queues_without_exception() {
-        let min_q:PriorityQueueLinkedList<u32, u32> =
-            PriorityQueueLinkedList::new(QueueType::MinQueue);
-        let max_q:PriorityQueueLinkedList<u32, u32> =
-            PriorityQueueLinkedList::new(QueueType::MaxQueue);
+        let max_q = create_empty_u32_max_queue();
+        let min_q = create_empty_u32_min_queue();
+    }
+
+    #[test]
+    fn empty_queue_has_length_zero() {
+        let (max_q, min_q) = (create_empty_u32_max_queue(), create_empty_u32_min_queue());
+        let (max_len, min_len) = (max_q.length(), min_q.length());
+        let expected_len = 0;
+        assert_eq!(max_len, expected_len);
+        assert_eq!(min_len, expected_len);
     }
 }
