acde6cc236e62612cd7131c6647fb10d8f2ab82b
data-pup <kevinmartinucr@gmail.com>
2018-03-24 18:19:56 -0700

    Added new scaffold to the linked list implementation of my priority queue.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+pub mod priority_queue_ll;
+
 enum QueueType { MinQueue, MaxQueue }
 
 trait Queue<V, P>
     where P: PartialOrd
 {
-    fn new(q_type:QueueType) -> Self;
     fn push(&mut self, value:V, priority:P);
     fn pop(&mut self) -> Option<V>;
     fn length(&self) -> u32;
---
+++ src/priority_queue_ll/mod.rs
@@ -0,0 +1,61 @@
+use std::cmp::PartialOrd;
+
+use Queue;
+use QueueType;
+
+/// Single node for the priority queue linked list implementation.
+struct LlNode<V, P>
+    where P:PartialOrd
+{
+    value:V,
+    priority:P,
+    next:Option<Box<LlNode<V, P>>>,
+}
+
+/// Priority Queue, using a linked list implementation.
+struct PriorityQueueLinkedList<V, P>
+    where P:PartialOrd
+{
+    head:Option<Box<LlNode<V, P>>>,
+    queue_type:QueueType,
+}
+
+impl<V, P> PriorityQueueLinkedList<V, P>
+    where P: PartialOrd
+{
+    pub fn new(queue_type:QueueType) -> Self {
+        PriorityQueueLinkedList {
+            head:None,
+            queue_type:queue_type,
+        }
+    }
+}
+
+impl<V, P> Queue<V, P> for PriorityQueueLinkedList<V, P>
+    where P: PartialOrd
+{
+    fn push(&mut self, value:V, priority:P) {
+    }
+
+    fn pop(&mut self) -> Option<V> {
+        None // FIXUP
+    }
+    fn length(&self) -> u32 {
+        0 // FIXUP
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use QueueType;
+    use priority_queue_ll;
+    use priority_queue_ll::PriorityQueueLinkedList;
+
+    #[test]
+    fn create_empty_queues() {
+        let min_q:PriorityQueueLinkedList<u32, u32> =
+            PriorityQueueLinkedList::new(QueueType::MinQueue);
+        let max_q:PriorityQueueLinkedList<u32, u32> =
+            PriorityQueueLinkedList::new(QueueType::MaxQueue);
+    }
+}
