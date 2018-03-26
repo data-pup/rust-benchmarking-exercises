933f73ee1043cde6a72ca03cf344bfeeabf4f456
data-pup <kevinmartinucr@gmail.com>
2018-03-26 09:14:11 -0700

    Came up with a better idea of what to benchmark, this will be documented next.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-pub mod priority_queue_ll;
-
-pub enum QueueType { MinQueue, MaxQueue }
-
-trait Queue<V, P>
-    where P: PartialOrd
-{
-    fn push(&mut self, value:V, priority:P);
-    fn pop(&mut self) -> Option<V>;
-    fn length(&self) -> u32;
-}
 
 #[cfg(test)]
 mod tests {
