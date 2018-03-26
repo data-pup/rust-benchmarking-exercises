9528a1b4828252123df209cdd5a1840514a67725
data-pup <kevinmartinucr@gmail.com>
2018-03-26 10:59:20 -0700

    Removed duplicate test from lib.rs. This is now in the tests/ directory.

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,3 @@ extern crate ndarray;
 
 pub mod multiply_naive;
 pub mod multiply_fast;
-
-#[cfg(test)]
-mod tests {
-    use multiply_naive::hello_world;
-    #[test]
-    fn it_works() {
-        let expected_res = "Hello World!";
-        let actual_res = hello_world();
-        assert_eq!(actual_res, expected_res);
-    }
-}
